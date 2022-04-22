49005b34d9d9c53f0460ea2117ec64fd11c7c3e3
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2022-04-23 00:41:28 +0200

    Добавил решение остальных частей

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,9 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+<<<<<<< HEAD
   return Object.fromEntries(Object.entries(obj).filter(([key, value]) => fields.includes(key)));  
+=======
+  return Object.fromEntries(Object.entries(obj).filter(([key, value]) => fields.includes(key))); 
+>>>>>>> Добавил решение остальных частей
 };
