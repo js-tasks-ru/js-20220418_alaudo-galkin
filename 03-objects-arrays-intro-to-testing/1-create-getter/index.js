1b308add8f1f6f09cc524eb2a660d33649ee0b75
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2022-04-28 22:07:57 +0200

    Решения остальных задач

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,35 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  function pickPart(string, part, separator = ".") {
+    let parts = string.split(separator);
+    if (part <= parts.length) {
+      return parts[part];
+    } else {
+      return string;
+    }
+  }
 
+  function skipParts(string, skipcount, separator = ".") {
+    let parts = string.split(separator);
+    if (skipcount < parts.length) {
+      return (parts.filter((a, i) => i >= skipcount)).join(separator);
+    }
+  }
+
+  function findProperty(path, object) {
+    if (typeof object === 'object') {
+      if (path.includes("."))
+      {
+        return findProperty(skipParts(path, 1), object[pickPart(path, 0)]); 
+      }
+      else
+      {
+        return object[path];
+      }
+    }
+  }
+    
+  return findProperty.bind(undefined, path);
 }
+
