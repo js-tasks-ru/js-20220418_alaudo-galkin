5e7dac119c20e6219c46aab1fd37f70001647c19
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2022-05-09 16:30:02 +0200

    Implemented components

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,59 @@
 export default class NotificationMessage {
-
-}
+    constructor (message, { duration = 1000, type = 'success' } = {})
+    {
+      this._checkInstance();
+  
+      this.message = message;
+      this.duration = duration;
+      this.type = type;
+      this._render();
+  
+      NotificationMessage.current = this;
+    }
+  
+    _checkInstance() {
+      if (NotificationMessage.current) {
+        NotificationMessage.current.destroy();
+      }
+    }
+  
+    _render () {
+      const wrapper = document.createElement("div");
+      wrapper.innerHTML = 
+      `<div class="notification ${this.type}" style="--value:${Math.floor(this.duration / 1000)}s">
+          <div class="timer"></div>
+          <div class="inner-wrapper">
+              <div class="notification-header">${this.type}</div>
+              <div class="notification-body">
+                  ${this.message}
+              </div>
+          </div>
+      </div>`;
+  
+      this.element = wrapper.firstChild;
+    }
+  
+    show (element) {
+      if (element) {
+        element.append(this.element);
+      } else {
+        document.body.prepend(this.element);
+      }
+      this.timeout = setTimeout(() => {
+        this.remove();
+      }, this.duration);
+    }
+  
+    remove () {
+      if (this.timeout) { 
+        clearTimeout(this.timeout); 
+      }
+      this.element?.remove();
+    }
+  
+    destroy() {
+      this.remove();
+      this.element = null;
+    }
+  }
+  
